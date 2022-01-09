2add421ec73971a775eefbfd4978aa85c507a967
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-01-09 17:58:06 +0100

    New split MESH test

---
+++ tests/bf_mesh_export/__init__.py
@@ -0,0 +1,18 @@
+import os
+from lib.bl_io import blend_tree_to_fds
+
+BL_PATH = "./bl/"
+EXCLUDE_DIRS = None
+EXCLUDE_FILES = None
+REF_PATH = "./ref/"
+
+
+def run():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    return blend_tree_to_fds(
+        package=__package__,
+        path=os.path.join(current_path, BL_PATH),
+        exclude_dirs=EXCLUDE_DIRS,
+        exclude_files=EXCLUDE_FILES,
+        ref_path=os.path.join(current_path, REF_PATH),
+    )
