9546913e4d1c0edd7bf0139958c0ce8c5a7bd3eb
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-01-11 20:11:49 +0100

    Update tests

--- tests/fds_validation_to_bf/__init__.py
+++ tests/fds_validation_to_bf/__init__.py
@@ -1,7 +1,7 @@
 import os
 from lib.bl_io import fds_tree_to_blend
 
-FDS_CASES_PATH = "../../../firemodels/fds/Validation/"
+FDS_PATH = "../../../firemodels/fds/Validation/"
 EXCLUDE_DIRS = ("Crown_Fires",)
 EXCLUDE_FILES = None
 
@@ -10,7 +10,7 @@ def run():
     current_path = os.path.dirname(os.path.abspath(__file__))
     return fds_tree_to_blend(
         package=__package__,
-        path=os.path.join(current_path, FDS_CASES_PATH),
+        path=os.path.join(current_path, FDS_PATH),
         exclude_dirs=EXCLUDE_DIRS,
         exclude_files=EXCLUDE_FILES,
     )
---
+++ tests/fds_verification_bad_geom_to_bf/__init__.py
@@ -0,0 +1,46 @@
+import os, sys, bpy
+from lib import TestFail, TestOk
+from lib.bl_io import fds_case_to_blend
+
+# BlenderFDS = sys.modules["blenderfds"]
+# ERR = BlenderFDS.types.BFException
+
+FDS_PATH = "../../../firemodels/fds/Verification/Complex_Geometry/"
+BAD_FILES = (
+    (
+        "geom_bad_open_surface.fds",
+        "Cube_open: Bad geometry: Non manifold or open geometry detected (4 edges).",
+    ),
+    (
+        "geom_bad_non_manifold_edge.fds",
+        "Cube_non_manifold_edge: Bad geometry: Non manifold vertices detected (2 vertices).",
+    ),
+    (
+        "geom_bad_inconsistent_normals.fds",
+        "Cube_inconsistent_normals: Bad geometry: Inconsistent face normals detected (4 edges).",
+    ),
+    (
+        "geom_bad_non_manifold_vert.fds",
+        "Cube_non_manifold_vert: Bad geometry: Non manifold vertices detected (2 vertices).",
+    ),
+    ("geom_bad_inverted_normals.fds", "Message!"),  # FIXME this should break
+)
+
+# BlenderFDS imports bad geometry, but raises BFException on export
+
+
+def run():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    results = list()
+    for filename, expected_msg in BAD_FILES:
+        results.extend(
+            fds_case_to_blend(
+                package=__package__,
+                filepath=os.path.join(current_path, FDS_PATH, filename),
+                expected_msg=None,
+                to_fds_expected_msg=expected_msg,
+                ref_path=None,
+                run_fds=False,
+            )
+        )
+    return results
