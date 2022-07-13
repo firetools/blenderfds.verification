678b66e517dcca9440245a7f28990372982d970c
Emanuele Gissi <emanuele.gissi@gmail.com>
2022-07-13 15:17:02 +0200

    Add multiple REAC import to free text

---
+++ tests/REAC_import/__init__.py
@@ -0,0 +1,27 @@
+"""!
+Test importing the REAC namelist (one and many)
+from fds cases to Blender files, export them again to fds,
+and compare the result with a reference.
+"""
+
+import os
+from lib.bl_io import fds_tree_to_blend
+from lib import config
+
+FDS_PATH = "./fds/"
+EXCLUDE_DIRS = None
+EXCLUDE_FILES = None
+REF_PATH = "./fds_ref/"
+
+
+def run():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    return fds_tree_to_blend(
+        package=__package__,
+        path=os.path.join(current_path, FDS_PATH),
+        exclude_dirs=EXCLUDE_DIRS,
+        exclude_files=EXCLUDE_FILES,
+        ref_path=os.path.join(current_path, REF_PATH),
+        run_fds=config.RUN_FDS,
+        set_ref=config.SET_REF,
+    )
